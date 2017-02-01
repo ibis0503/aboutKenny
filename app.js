c287de88120b8f28c1b5f92550f90a202bc240f4
James R Billard <jamesrbillard@Jamess-MacBook-Pro.local>
2017-01-31 21:05:52 -0800

    Finished function 1 - 4

--- app.js
+++ app.js
@@ -1,57 +1,75 @@
-//Ask User's Name//
-var UserName = prompt ('What is your name?');
-alert ('Welcome ' + UserName + '!');
-console.log('Welcome ' + UserName);
-//Count the correct answers from the users//
+function name (){
+
+  //Ask User's Name//
+  var UserName = prompt ('What is your name?');
+  alert ('Welcome ' + UserName + '!');
+  console.log('Welcome ' + UserName);
+  //Count the correct answers from the users//
+};
 var correctAnswer = 0;
-//Question One //
-var answerOne = prompt('Q1. Kenny\'s currently working at Xbox Team (y/n or yes/no)');
-// Ask users question with input//
-if (answerOne.toUpperCase() === 'NO' || answerOne.toUpperCase() === 'N' || answerOne.toUpperCase() === 'NOPE' || answerOne.toUpperCase() === 'NAH') {
-  // This console.log will execute as long as one of all four conditions evaluates to 'True'//
-  alert('You got it right! ');
-  console.log('You got it right!');
-  correctAnswer += 1;
-} else {
-  alert('Wrong!!! ');
-  console.log('Wrong!');
-}
-//Question Two //
-var answerTwo = prompt('Q2. Kenny grauated from Harvard Law School (y/n or yes/no)');
-// Ask users question with input
-if (answerTwo.toUpperCase() === 'NO' || answerTwo.toUpperCase() === 'N' || answerTwo.toUpperCase() === 'NOPE' || answerTwo.toUpperCase() === 'NAH') {
-  // This console.log will execute as long as one of all four conditions evaluates to 'True'//
-  alert('You got it right! ');
-  console.log('You got it right!');
-  correctAnswer += 1;
-} else {
-  alert('Wrong!!! ');
-  console.log('Wrong!');
-}
-//Question Three //
-var answerThree = prompt('Q3. Kenny recently graduated from college (y/n or yes/no)');
-// Ask users question with input
-if (answerThree.toUpperCase() === 'NO' || answerThree.toUpperCase() === 'N' || answerThree.toUpperCase() === 'NOPE' || answerThree.toUpperCase() === 'NAH') {
-  // This console.log will execute as long as one of all four conditions evaluates to 'True'//
-  alert('You got it right! ');
-  console.log('You got it right!');
-  correctAnswer += 1;
-} else {
-  alert('Wrong!!! ');
-  console.log('Wrong!');
-}
-//Question Four //
-var answerFour = prompt('Q4. Kenny has bachelor\'s degree in Human Centered Design & Engineering  (y/n or yes/no)');
-// Ask users question with input
-if (answerFour.toUpperCase() === 'NO' || answerFour.toUpperCase() === 'N' || answerFour.toUpperCase() === 'NOPE' || answerFour.toUpperCase() === 'NAH') {
-  // This console.log will execute as long as one of all four conditions evaluates to 'True'//
-  alert('You got it right! ');
-  console.log('You got it right!');
-  correctAnswer += 1;
-} else {
-  alert('Wrong!!! ');
-  console.log('Wrong!');
-}
+function questionOne() {
+
+  //Question One //
+  var answerOne = prompt('Q1. Kenny\'s currently working at Xbox Team (y/n or yes/no)');
+  // Ask users question with input//
+  if (answerOne.toUpperCase() === 'NO' || answerOne.toUpperCase() === 'N' || answerOne.toUpperCase() === 'NOPE' || answerOne.toUpperCase() === 'NAH') {
+    // This console.log will execute as long as one of all four conditions evaluates to 'True'//
+    alert('You got it right! ');
+    console.log('You got it right!');
+    correctAnswer += 1;
+  } else {
+    alert('Wrong!!! ');
+    console.log('Wrong!');
+  }
+};
+
+function questionTwo(){
+
+  //Question Two //
+  var answerTwo = prompt('Q2. Kenny grauated from Harvard Law School (y/n or yes/no)');
+  // Ask users question with input
+  if (answerTwo.toUpperCase() === 'NO' || answerTwo.toUpperCase() === 'N' || answerTwo.toUpperCase() === 'NOPE' || answerTwo.toUpperCase() === 'NAH') {
+    // This console.log will execute as long as one of all four conditions evaluates to 'True'//
+    alert('You got it right! ');
+    console.log('You got it right!');
+    correctAnswer += 1;
+  } else {
+    alert('Wrong!!! ');
+    console.log('Wrong!');
+  }
+};
+
+function questionThree(){
+
+  //Question Three //
+  var answerThree = prompt('Q3. Kenny recently graduated from college (y/n or yes/no)');
+  // Ask users question with input
+  if (answerThree.toUpperCase() === 'NO' || answerThree.toUpperCase() === 'N' || answerThree.toUpperCase() === 'NOPE' || answerThree.toUpperCase() === 'NAH') {
+    // This console.log will execute as long as one of all four conditions evaluates to 'True'//
+    alert('You got it right! ');
+    console.log('You got it right!');
+    correctAnswer += 1;
+  } else {
+    alert('Wrong!!! ');
+    console.log('Wrong!');
+  }
+};
+
+function questionFour(){
+
+  //Question Four //
+  var answerFour = prompt('Q4. Kenny has bachelor\'s degree in Human Centered Design & Engineering  (y/n or yes/no)');
+  // Ask users question with input
+  if (answerFour.toUpperCase() === 'NO' || answerFour.toUpperCase() === 'N' || answerFour.toUpperCase() === 'NOPE' || answerFour.toUpperCase() === 'NAH') {
+    // This console.log will execute as long as one of all four conditions evaluates to 'True'//
+    alert('You got it right! ');
+    console.log('You got it right!');
+    correctAnswer += 1;
+  } else {
+    alert('Wrong!!! ');
+    console.log('Wrong!');
+  }
+};
 //Question Five //
 var questionFive = prompt('Q5. Which are the organization that Kenny hasn\'t worked previously?  A. Microsoft  B.Best Buy  C. Seattle Seahawks  D. University of Washington');
 // Ask users question with input to find out whether user's input is in an array
@@ -91,7 +109,7 @@ var answerSeven = ['KOREA', 'CALIFORNIA'];
 do {
   userAnswerAttempts += 1;
   var userAnswerSeven = prompt('Q7. Can you guess where I was from?');
-// Ask users question to guess input inside of strings, and tell users whether their answer is correct or not in six times//
+  // Ask users question to guess input inside of strings, and tell users whether their answer is correct or not in six times//
   if (answerSeven.indexOf(userAnswerSeven.toUpperCase()) !== -1) {
     console.log ('Yes,I\'m from ' + userAnswerSeven.toUpperCase());
     alert ('You got it right! \nI\'m from ' + userAnswerSeven);
